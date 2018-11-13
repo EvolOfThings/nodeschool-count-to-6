292951231de1204041c19ea78a32dbca61098f96
RishadYamnoor <rishad.yamnoor@gmail.com>
2018-11-13 21:38:06 +0530

    Initial commit

---
+++ program.js
@@ -0,0 +1,15 @@
+//#1
+//console.log("HEELO ES6");
+
+//#2 - Template strings
+// console.log(`Hello, ${process.argv[2]}!
+// Your name lowercased is "${process.argv[2].toLowerCase()}".`);
+
+//#3 - Arrow Functions Part 1
+// var inputs = process.argv.slice(2);
+// var results = inputs.map(letter => letter[0])
+//                     .reduce((last, first) => last + first);
+// console.log(`[${inputs}] becomes "${results}"`);
+
+//#4 - Arrow Functions and This
+
