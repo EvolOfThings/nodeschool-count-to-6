4c4cda5b9919be5c71a073811212e4fe375384b3
RishadYamnoor <rishad.yamnoor@gmail.com>
2018-11-15 19:17:08 +0530

    for loop

--- for-loop.js
+++ for-loop.js
@@ -14,3 +14,13 @@
 //     console.log(`The fruit name has more than five characters.`);
 // } else
 //     console.log(`The fruit name has five characters or less.`);
+
+//for-loop
+
+let total = 0;
+let limit = 10;
+for (var i = 0; i < limit; i++) {
+   total += i;
+}
+
+   console.log(total);
